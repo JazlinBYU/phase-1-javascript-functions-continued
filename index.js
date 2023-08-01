60edf47bf4f71d2ea1483fc76145dad4743674fd
Jazlin Yu <jazlinsub@gmail.com>
2023-08-01 13:06:56 -0700

    passing test

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+/** @format */
+
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(wrapper = "*") {
+  // Implement the function logic here
+  return function (adjective) {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
